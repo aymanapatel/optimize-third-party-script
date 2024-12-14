1a27d5b21b1e49d9bc90fed56d56964fa45f1df8
Ayman Patel <ayman.patel97@outlook.com>
2024-12-14 22:04:50 +0530

    feat: Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import mixPanel from './assets/mixpanel.png'
+import amplitude from './assets/amplitude.png'
+
 import './App.css'
 import Login from './components/Login'
 
@@ -8,14 +9,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <img src={mixPanel} className="logo mixpanel" alt="mixpanel" />
+      <img src={amplitude} className="logo amplitude" alt="amplitude" />
+
       </div>
-      <h1>Partytown demo</h1>
+      <h1>Partytown demo without Partytown</h1>
       <Login/>
     </>
   )
